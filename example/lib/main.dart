15b6033c20ac1ff9a5fd3c33cc7b6a2a6ca02734
birjuvachhani <brvachhani@gmail.com>
2023-10-03 11:09:32 +0530

    :recycle: update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,11 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:adaptive_theme/adaptive_theme.dart';
-import 'package:example/cupertino_example.dart';
-import 'package:example/material_example.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,29 +11,77 @@ void main() async {
   runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   final AdaptiveThemeMode? savedThemeMode;
 
   const MyApp({super.key, this.savedThemeMode});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.blue,
+      ),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.blue,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'Adaptive Theme Demo',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const MyHomePage(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  bool isMaterial = true;
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: const Duration(seconds: 1),
-      child: isMaterial
-          ? MaterialExample(
-              savedThemeMode: widget.savedThemeMode,
-              onChanged: () => setState(() => isMaterial = false))
-          : CupertinoExample(
-              savedThemeMode: widget.savedThemeMode,
-              onChanged: () => setState(() => isMaterial = true)),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adaptive Theme Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'This is a sample app to demonstrate the usage of adaptive theme.',
+            ),
+            const Text(
+              'You can switch between light and dark theme using the switch below.',
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Light'),
+                const SizedBox(width: 10),
+                Switch(
+                  value: AdaptiveTheme.of(context).mode.isDark,
+                  onChanged: (value) {
+                    if (value) {
+                      AdaptiveTheme.of(context).setDark();
+                    } else {
+                      AdaptiveTheme.of(context).setLight();
+                    }
+                  },
+                ),
+                const SizedBox(width: 10),
+                const Text('Dark'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ example/lib/playground.dart
@@ -0,0 +1,45 @@
+// Copyright © 2020 Birju Vachhani. All rights reserved.
+// Use of this source code is governed by an Apache license that can be
+// found in the LICENSE file.
+
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:example/cupertino_example.dart';
+import 'package:example/material_example.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
+}
+
+class MyApp extends StatefulWidget {
+  final AdaptiveThemeMode? savedThemeMode;
+
+  const MyApp({super.key, this.savedThemeMode});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isMaterial = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(seconds: 1),
+      child: isMaterial
+          ? MaterialExample(
+              savedThemeMode: widget.savedThemeMode,
+              onChanged: () => setState(() => isMaterial = false),
+            )
+          : CupertinoExample(
+              savedThemeMode: widget.savedThemeMode,
+              onChanged: () => setState(() => isMaterial = true),
+            ),
+    );
+  }
+}
