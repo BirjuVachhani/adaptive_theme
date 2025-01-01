c73c8d663d5688874c2944267060c721b781c120
BirjuVachhani <brvachhani@gmail.com>
2025-01-01 12:06:30 +0530

    :fire: remove unnecessary library name

--- lib/adaptive_theme.dart
+++ lib/adaptive_theme.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an Apache license that can be
 // found in the LICENSE file.
 
-library adaptive_theme;
+library;
 
 export 'src/adaptive_theme.dart';
 export 'src/adaptive_theme_manager.dart';
